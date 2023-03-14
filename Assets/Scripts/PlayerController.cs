2a5f11af2786578c87e6670eda9e14fb680c66eb
mike-935 <Michael935935@gmail.com>
2023-03-14 17:09:18 -0400

    Hopefully fixed mikes room

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    public float speed = 10f;
+    public float jumpHeight = 10f;
+    public float gravity = 9.81f;
+    public float airControl = 10f;
+    private Vector3 input;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        input = (transform.right * moveHorizontal+ transform.forward * moveVertical).normalized;
+        input *= speed;
+
+       
+
+        if (controller.isGrounded)
+        {
+
+            moveDirection = input;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else
+            {
+                moveDirection.y = 0.0f;
+            }
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(input * Time.deltaTime);
+    }
+}
